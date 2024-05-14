a55055e5fd584dba66d96fa0bc0cfe33e596af13
rh <rvhoyos@gmail.com>
2024-05-13 22:28:03 -0400

    Added randomNumberGenerator.py to implement as a strategy pattern into the airport-simulation model as simulation inputs. This is the final refactor in parametrizing the simulator and simulation model.

---
+++ src/airport/RVG/randomNumberGenerator.py
@@ -0,0 +1,34 @@
+from abc import ABC, abstractmethod
+import numpy as np
+
+
+# todo refactor to use this class in the aiport.py and related classes.
+class RandomNumberGenerator(ABC):
+    @abstractmethod
+    def generate(self):
+        pass
+
+
+class ExponentialRandomNumberGenerator(RandomNumberGenerator):
+    def __init__(self, rate):
+        self.rate = rate
+
+    def generate(self):
+        return np.random.exponential(1 / self.rate)  # todo checkinCounter has 1/rate check if correct in broader use.
+
+
+class NormalRandomNumberGenerator(RandomNumberGenerator):
+    def __init__(self, mean, variance):
+        self.mean = mean
+        self.variance = variance
+
+    def generate(self):
+        return np.random.normal(self.mean, np.sqrt(self.variance))
+
+
+class GeometricRandomNumberGenerator(RandomNumberGenerator):
+    def __init__(self, p):
+        self.p = p
+
+    def generate(self):
+        return np.random.geometric(p=self.p) - 1  # Subtracting 1 because geometric distribution starts at 1
